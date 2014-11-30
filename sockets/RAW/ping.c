bb9e56b4ab341f77cfdc9e1630b569b9f752e12e
Your Name <you@example.com>
2014-11-30 23:38:05 +0300

    add getopt for some of cli args

--- sockets/RAW/ping.c
+++ sockets/RAW/ping.c
@@ -10,6 +10,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+#include <string.h>
+#include <pthread.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <ctype.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -17,10 +22,6 @@
 #include <netdb.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/ip.h>
-#include <string.h>
-#include <pthread.h>
-#include <setjmp.h>
-#include <signal.h>
 
 #if DEBUG > 0
 #define pr_dbg(...) fprintf(stderr, __VA_ARGS__)
@@ -28,11 +29,14 @@
 #define pr_dbg(...) ;
 #endif
 
-#define DELAY 8050
+#define MAX_NAME_SIZ 256
+#define DELAY 500000
 #define BUF_SIZ 1000
 #define LOAD_SIZ 5
 
 struct stuff {
+	useconds_t delay;
+	char hname[MAX_NAME_SIZ];
 	int recv_sock, send_sock;
 	struct sockaddr_in recv_sockaddr, send_sockaddr;
 	struct hostent *h_ent;
@@ -43,7 +47,9 @@ struct s_frame {
 	char payload[LOAD_SIZ];
 };
 
-int ping(const char *name);
+void usage(int st);
+int ping4(struct stuff *conn);
+int ping6(struct stuff *conn);
 int routines(struct stuff *conn);
 unsigned short in_cksum(unsigned short *addr, size_t len);
 void pr_bytes(const char *str, int size);
@@ -53,40 +59,123 @@ jmp_buf env;
 
 int main(int argc, char *argv[])
 {
-	pr_dbg("pr_dbg is works\n");
-	if (argc < 2) {
-		fprintf(stderr, "need hostname\n");
-		exit(1);
-	}
-	if (ping(argv[1])) {
-		printf("host %s is alive\n", argv[1]);
-		return 0;
-	} else {
-		printf("host %s is not alive\n", argv[1]);
+	int res, i;
+	struct stuff *conn = NULL;
+	const char optstring[] = "hp:qd:H:";
+	char q_fl = 0, h_fl = 0 proto = 4;
+	if (argc < 3) {
+		usage(0);
 		return 1;
 	}
-}
-
-int ping(const char *name)
-{
-	void *succ_cnt;
-	struct stuff *conn;
-	pthread_t routin_th;
-	int th_result;
 	conn = malloc(sizeof(struct stuff));
 	if (!conn) {
 		perror("malloc: ");
-		return 0;
+		return 1;
 	}
-	conn->h_ent = gethostbyname(name);
+	conn->delay = DELAY;
+	for (;;) {
+		res = getopt(argc, argv, optstring);
+		if (res == -1) {
+			break;
+		}
+		switch (res) {
+			case 'd':
+				for (i = 0; optarg[i]; i++) {
+					if (!isdigit((int)optarg[i] & 0xff)) {
+						usage(0);
+						res = 1;
+						goto exit;
+					}
+				}
+				conn->delay = atoi(optarg);
+				break;
+			case 'h':
+				usage(1);
+				goto exit;
+				break;
+			case 'H':
+				strcpy(conn->hname, optarg);
+				h_fl = 1;
+				break;
+			case 'p':
+				if (strcmp(optarg, "6") == 0 ||
+						strcmp(optarg, "4") == 0) {
+					proto = atoi(optarg);
+				} else {
+					res = 1;
+					usage(0);
+					goto exit;
+				}
+				break;
+			case 'q':
+				q_fl = 1;
+				break;
+			case '?':
+				fprintf(stderr, "unrecognized option: %c\n",
+						res);
+				res = 1;
+				usage(0);
+				goto exit;
+				break;
+			default:
+				fprintf(stderr, "getopt return code 0x%x\n",
+						res & 0xff);
+				res = 1;
+				usage(0);
+				goto exit;
+				break;
+		}
+	}
+	if (!h_fl) {
+		usage(0);
+		res = 1;
+		goto exit;
+	}
+	if (proto == 6) {
+		fprintf(stderr, "ipv6 under construction\n");
+		res = 1;
+		goto exit;
+		if (ping6(conn))
+			goto succ;
+		else
+			goto fail;
+	} else {
+		if (ping4(conn))
+			goto succ;
+		else
+			goto fail;
+	}
+succ:
+	res = 0;
+	if (q_fl == 0) {
+		printf("host %s is alive\n", conn->hname);
+	}
+	goto exit;
+fail:
+	res = 1;
+	if (q_fl == 0) {
+		printf("host %s is not alive\n", conn->hname);
+	}
+exit:
+	free(conn);
+	exit(res);
+}
+
+int ping4(struct stuff *conn)
+{
+	struct sigaction act;
+	void *succ_cnt;
+	pthread_t routin_th;
+	int th_result;
+	conn->h_ent = gethostbyname(conn->hname);
 	if (!conn->h_ent) {
 		perror("gethostbyname: ");
-		goto bad;
+		return 0;
 	}
 	if ( (conn->send_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) ==
 			-1) {
 		perror("socket: ");
-		goto bad;
+		return 0;
 	}
 	conn->send_sockaddr.sin_family = AF_INET;
 	conn->send_sockaddr.sin_addr.s_addr =
@@ -94,13 +183,12 @@ int ping(const char *name)
 	if ( (conn->recv_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) ==
 			-1) {
 		perror("socket: ");
-		goto bad;
+		return 0;
 	}
 	conn->recv_sockaddr.sin_family = AF_INET;
 	conn->recv_sockaddr.sin_addr.s_addr = *(long *)conn->
 		h_ent->h_addr_list[0];
 	/* set sighandler */
-	struct sigaction act;
 	memset(&act, 0, sizeof(act));
 	act.sa_handler = sighndlr;
 	sigaction(SIGUSR1, &act, 0);
@@ -109,28 +197,24 @@ int ping(const char *name)
 			(void *)conn);
 	if (th_result != 0) {
 		perror("!!!!!ptread: ");
-		goto bad;
+		return 0;
 	}
-	usleep(DELAY);
+	usleep(conn->delay);
 	/* send SIGUSR1 to thread */
 	pthread_kill(routin_th, SIGUSR1);
 	/* get the number of sucessful ping */
 	if (pthread_join(routin_th, &succ_cnt) == 0) {
 		if (((long)succ_cnt) >= 3) {
 			pr_dbg("total ping recieved: %lu\n", (long)succ_cnt);
-			free(conn);
 			return 1;
 		} else {
 			pr_dbg("total ping recieved: %lu\n", (long)succ_cnt);
-			goto bad;
+			return 0;
 		}
 	} else {
 		perror("pthread_joint: ");
-		goto bad;
+		return 0;
 	}
-bad:
-	free(conn);
-	return 0;
 }
 
 void sighndlr(int sig)
@@ -286,6 +370,7 @@ cont:
 #endif
 		pr_dbg("next sequence is %hu\n=============\n", seqtmp);
 	}
+
 end:
 	/* set block for SIGUSR1 */
 	sigemptyset(&set);
@@ -298,6 +383,7 @@ end:
 	pthread_exit(succ_cnt);
 }
 
+#if DEBUG > 1
 void pr_bytes(const char *str, int size)
 {
 	unsigned char hex[] = {'0','1','2','3','4','5',
@@ -323,6 +409,7 @@ void pr_bytes(const char *str, int size)
 	}
 	fprintf(stderr, "\n");
 }
+#endif
 
 unsigned short in_cksum(unsigned short *addr, size_t len)
 {
@@ -347,3 +434,10 @@ unsigned short in_cksum(unsigned short *addr, size_t len)
 	return(answer);
 }
 
+void usage(int st)
+{
+	if (st) 
+		printf("usage:./a.out [-h] -H localhost [-p 4|6] [-q] [-d]\n");
+	else
+		fprintf(stderr, "usage:./a.out [-h] -H localhost [-p 4|6] [-q] [-d delay in nanosec]\n");
+}
